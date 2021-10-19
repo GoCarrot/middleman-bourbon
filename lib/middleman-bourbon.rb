2661f1a17518af5f86341ae86e81fe39eca90049
Alex Scarborough <alex@teak.io>
2021-10-19 14:10:08 -0700

    Maybe working with Middleman 4.

--- lib/middleman-bourbon.rb
+++ lib/middleman-bourbon.rb
@@ -1,20 +1,19 @@
 require 'middleman-bourbon/version'
 
-module MiddlemanBourbon
+require 'middleman-bourbon/version'
 
-  class << self
-    def registered(app)
-      require "bourbon"
+class MiddlemanBourbon < Middleman::Extension
+  def initialize(app, options_hash = {}, &block)
+    require "bourbon"
 
-      gem_dir = Gem::Specification.find_by_name("bourbon").gem_dir
+    gem_dir = Gem::Specification.find_by_name("bourbon").gem_dir
 
-      # add bourbon to sass load path
-      Sass.load_paths << File.expand_path("./app/assets/stylesheets", gem_dir)
+    # add bourbon to sass load path
+    Sass.load_paths << File.expand_path("./app/assets/stylesheets", gem_dir)
 
-      puts "Bourbon loaded"
-    end
-    alias :included :registered
+    puts "Bourbon loaded"
   end
+  alias :included :registered
 end
 
 ::Middleman::Extensions.register(:bourbon, MiddlemanBourbon)
